ff097593376e360a709de32639f4a5b0cfa44406
Triasa Das <triasa312@gmail.com>
2024-09-03 15:08:20 +0530

    Enc

--- encryption/temp1.py
+++ encryption/temp1.py
@@ -1,9 +1,12 @@
 import pyzipper
+import getpass
+
+# Take the password as user input securely
+password = getpass.getpass(prompt='Set password: ').encode('utf-8')
 
 # Define the file to be encrypted and the name of the output zip file
-file_to_encrypt = '/Users/architanant/Documents/Quantech/encryption/file.txt'
+file_to_encrypt = '/Users/macbook/Documents/Quantech/encryption/file.txt'
 zip_file = 'encrypted_example.zip'
-password = print(b"set pass.")  # Define your password here
 
 # Create a new zip file and add the file to it, encrypting with the password
 with pyzipper.AESZipFile(zip_file, 'w', compression=pyzipper.ZIP_LZMA) as zf:
@@ -11,17 +14,26 @@ with pyzipper.AESZipFile(zip_file, 'w', compression=pyzipper.ZIP_LZMA) as zf:
     zf.setencryption(pyzipper.WZ_AES, nbits=256)  # AES encryption with 256-bit key
     zf.write(file_to_encrypt)
 
-print("File encrypted and saved as a zip archive successfully")
+print("File encrypted and saved as a zip archive successfully.")
 
 
-# Define the name of the encrypted zip file and the password
+# Take the password as user input securely
+password = getpass.getpass(prompt='Enter password: ').encode('utf-8')
+
+# Define the name of the encrypted zip file and the name of the output file
 zip_file = 'encrypted_example.zip'
-password = print("enter pass:")  # Use the same password used for encryption
+output_file = 'decrypted_example.txt'
 
 # Extract the encrypted file
 with pyzipper.AESZipFile(zip_file) as zf:
     zf.setpassword(password)
-    zf.extractall()
+    try:
+        zf.extractall()  # This will extract all files in the zip
+        print(f"File decrypted and extracted successfully.")
+    except RuntimeError:
+        print("Incorrect password or file is corrupted.")
 
-print("File decrypted and extracted successfully.")
+
+# Define the name of the encrypted zip file and the password
+
 
